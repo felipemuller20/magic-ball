708da6f7705e777714da069f9ea8a56684af313c
felipe <felipeavmuller@gmail.com>
2023-10-28 19:04:09 -0300

    create magicball project

---
+++ android/app/src/main/kotlin/com/gmailfelipeavmuller/magic_ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gmailfelipeavmuller.magic_ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[100],
+        appBar: AppBar(
+          title: Text('Magic Ball'),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: const MagicBall(),
+      ),
+    ),
+  );
+}
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({super.key});
+
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballResponse = 1;
+
+  void randomizeResponse() {
+    setState(() {
+      ballResponse = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          randomizeResponse();
+        },
+        child: Image.asset('assets/images/ball$ballResponse.png'),
+      ),
+    );
+  }
+}
